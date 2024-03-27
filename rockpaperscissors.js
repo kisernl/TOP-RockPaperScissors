b6e1a9b674a7e423052b3210297221ce54460229
Noah Kiser <noah.kiser@gmail.com>
2024-03-27 13:25:58 -0500

    Revised code to return string not integer needed getComputerChoice to ultimately lead to a starting not just a integer 1-3

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,10 +1,17 @@
 // create a function titled getComputerChoice
-let getComputerChoice = Math.floor((Math.random()*3) + 1)
+let getComputerChoice = Math.floor((Math.random()*3) + 1);
 // include three strings "Rock "Paper" "Scissors"
 // above code randoms chooses an integer 1, 2, or 3 
 // - need to assign Rock, Paper, & Scissor to these integers
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
-console.log(getComputerChoice);
+let computerSelection;
+if (getComputerChoice === 1){
+	computerSelection = "rock";
+} else if (getComputerChoice === 2){
+  computerSelection = "paper";
+} else if (getComputerChoice === 3){
+  computerSelection = "scissors";
+} else {
+   computerSelection = "Invalid Selection";
+}
+//use console to test
+console.log(computerSelection);
