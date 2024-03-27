c8b9806f3f715b7c27acc61ed21ac49766dc4c9b
Noah Kiser <noah.kiser@gmail.com>
2024-03-27 12:47:06 -0500

    Create getComputerChoice function made function to randomly select a number 1-3 assigned variables (rock, paper, & scissors) to these three integers

---
+++ rockpaperscissors.js
@@ -0,0 +1,10 @@
+// create a function titled getComputerChoice
+let getComputerChoice = Math.floor((Math.random()*3) + 1)
+// include three strings "Rock "Paper" "Scissors"
+// above code randoms chooses an integer 1, 2, or 3 
+// - need to assign Rock, Paper, & Scissor to these integers
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+console.log(getComputerChoice);
