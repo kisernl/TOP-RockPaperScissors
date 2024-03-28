536eb20f9f9c8f5edb0ee4a9edb5721d9a87e14c
Noah Kiser <noah.kiser@gmail.com>
2024-03-28 13:34:55 -0500

    Create user input prompt

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,18 @@
+
+
+// create a prompt function to collect user/player input/choice
+let choice = prompt("Make your choice: rock, paper, or scissors?");
+let playerSelection = choice.toLowerCase();
+
+if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+  alert("Good luck!");
+} else {
+  alert(`Invalid Input. Only "rock" or "paper" or "scissors" accepted.`);
+}
+// NEED to learn how to send the user back to the prompt until valid input received
+
+console.log(playerSelection)
+
 // create a function titled getComputerChoice
 let getComputerChoice = Math.floor((Math.random()*3) + 1);
 // include three strings "Rock "Paper" "Scissors"
@@ -17,9 +32,9 @@ if (getComputerChoice === 1){
 console.log(computerSelection);
 
 // create function that accepts two parameters
-let playerChoice;
+// let playerChoice;
 // let playerSelection = playerChoice.toLowerCase();
-function playRPSGame(playerSelection, computerSelection) {
+function playRPSGame(playerSelection, computerSelection) { 
 // return a string declaring the winner
 if (playerSelection === "rock" && computerSelection === "scissors"){
 return "You win! Rock beats Scissors";
@@ -41,6 +56,7 @@ return "Tie!";
 
 //test in console
   
-const playerSelection = "rock";
+// const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
 console.log(playRPSGame(playerSelection, computerSelection));
+
